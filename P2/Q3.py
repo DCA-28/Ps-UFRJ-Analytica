dbb2d97a553c3b90d421e70e9ce8cd8e92704659
Daniel Corcino de Albuquerque <danielcorcinodealbuquerque@gmail.com>
2021-05-30 13:45:51 -0300

    Q3

---
+++ P2/Q3.py
@@ -0,0 +1,58 @@
+class CashMachine:
+    def __init__(self):
+        self.cash_notes = [100, 50, 20, 10, 5, 2]
+        self.cash_coins = [1, 0.5, 0.25, 0.1, 0.05, 0.01]
+        self.withdraw_dict = {
+            100: 0,
+            50: 0,
+            20: 0,
+            10: 0,
+            5: 0,
+            2: 0,
+            1: 0,
+            0.5: 0,
+            0.25: 0,
+            0.1: 0,
+            0.05: 0,
+            0.01: 0,
+        }
+
+    def withdraw(self, total_value):
+        amount = total_value
+        # first let's see how many notes we need for the withdraw
+        for note in self.cash_notes:
+            units = amount // note
+            if units > 0:
+                amount -= units * note
+                self.withdraw_dict[note] += units
+        # converting the remaining to two decimal places
+        remaining_amount = amount
+        # and let's fetch the coins
+        for coin in self.cash_coins:
+            units = remaining_amount // coin
+            if units > 0:
+                remaining_amount -= units * coin
+                self.withdraw_dict[coin] += units
+            # print(remaining_amount)
+        return self.withdraw_dict
+
+    # getter method
+    def cash_returned(self, total_value):
+        print(f">> {total_value}", end="\n\n")
+        print("NOTAS:")
+        for note in self.cash_notes:
+            notes_quantity = int(self.withdraw_dict[note])
+            print(f"{notes_quantity} nota(s) de R$ {note:.2f}")
+        print("\nMOEDAS:")
+        for coin in self.cash_coins:
+            print(f"{self.withdraw_dict[coin]:.0f} moeda(s) de R$ {coin:.2f}")
+
+
+def main():
+    value = float(input())
+    cash_machine = CashMachine()
+    cash_machine.withdraw(value)
+    cash_machine.cash_returned(value)
+
+
+main()
