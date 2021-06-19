a9485208e2adba09b54b76630d0794746a0972ff
Daniel Corcino de Albuquerque <danielalbuquerque@dcc.ufrj.br>
2021-06-18 22:40:44 -0300

    Q2

--- P2/Q2.py
+++ P2/Q2.py
@@ -1,66 +1,30 @@
 import pandas as pd
 
-"talvez usar pd na criacao de um objeto chess table"
+""" Operators will be a list containing the indexes of all the virtually possible horse movements, for example the 
+    horse can go 2 places up and 1 place left, witch is accomplished by the operator: [2, -1]. That moviment is realized 
+    by adding these opperator numbers to the horizontal coordinate (hori_coord) and vertical coordinate (vert_coord). 
+    In that case we would have: final_vert = vert_coord + 2 and final_hori = hori_coord - 1. Given the starting position 
+    of the horse and using all the operators on that position, we have virtually all end positions. These end positions
+    are analyzed in the future to find only the valid ones."""
+
+""" The symbols on the board that will be printed indicate:
+    h -> the horse position
+    o -> one of the houses the horse can go
+    * -> the final position of the horse (when it exists)"""
 
 
 def all_moves(position: str) -> str:
-    hori_coord = position[
-        0
-    ]  # represents the letter in the position (horizontal coordinate)
-    vert_coord = position[
-        1
-    ]  # represents the number in the position (vertical coordinate)
+    hori_coord = position[0]  # represents the letter in the position (horizontal coordinate)
+    vert_coord = position[1]  # represents the number in the position (vertical coordinate)
     possible_coords = []
 
-    # goes two places up and one place left
-    final_vert = ord(vert_coord) + 2
-    final_hori = ord(hori_coord) - 1
-    final_cords = chr(final_hori) + chr(final_vert)
-    possible_coords.append(final_cords)
+    operators = [[2, -1], [2, 1], [1, -2], [1, 2], [-2, -1], [-2, 1], [-1, -2], [-1, 2]]
 
-    # goes two places up and one place rigth
-    final_vert = ord(vert_coord) + 2
-    final_hori = ord(hori_coord) + 1
-    final_cords = chr(final_hori) + chr(final_vert)
-    possible_coords.append(final_cords)
-
-    # goes one place up and two places left
-    final_vert = ord(vert_coord) + 1
-    final_hori = ord(hori_coord) - 2
-    final_cords = chr(final_hori) + chr(final_vert)
-    possible_coords.append(final_cords)
-
-    # goes one place up and two places rigth
-    final_vert = ord(vert_coord) + 1
-    final_hori = ord(hori_coord) + 2
-    final_cords = chr(final_hori) + chr(final_vert)
-    possible_coords.append(final_cords)
-
-    # goes two places down and one place left
-    final_vert = ord(vert_coord) - 2
-    final_hori = ord(hori_coord) - 1
-    final_cords = chr(final_hori) + chr(final_vert)
-    possible_coords.append(final_cords)
-
-    # goes two places down and one place rigth
-    final_vert = ord(vert_coord) - 2
-    final_hori = ord(hori_coord) + 1
-    final_cords = chr(final_hori) + chr(final_vert)
-    possible_coords.append(final_cords)
-
-    # goes one place down and two places left
-    final_vert = ord(vert_coord) - 1
-    final_hori = ord(hori_coord) - 2
-    final_cords = chr(final_hori) + chr(final_vert)
-    possible_coords.append(final_cords)
-
-    # goes one place down and two places rigth
-    final_vert = ord(vert_coord) - 1
-    final_hori = ord(hori_coord) + 2
-    final_cords = chr(final_hori) + chr(final_vert)
-    possible_coords.append(final_cords)
-
-    print(possible_coords)
+    for operator in operators:
+        final_vert = (ord(vert_coord) + operator[0])  # final vertical coordinate after applying the vertical move
+        final_hori = (ord(hori_coord) + operator[1])  # final horizontal coordinate after applying the vertical move
+        final_cords = chr(final_hori) + chr(final_vert)
+        possible_coords.append(final_cords)
     return possible_coords
 
 
@@ -69,15 +33,9 @@ def possible_moves(possible_coords: list) -> list:
     for coordinates in possible_coords:
         hori_coord = coordinates[0]
         verti_coord = coordinates[1]
-        if (
-            ord(hori_coord) > 104 or ord(hori_coord) < 97
-        ):  # whether the horse has crossed the left or right limit
-            print(f"Ops limit transpassed: {hori_coord}")
+        if (ord(hori_coord) > 104 or ord(hori_coord) < 97):  # whether the horse has crossed the left or right limit
             continue
-        elif (
-            ord(verti_coord) > 56 or ord(verti_coord) < 49
-        ):  # whether the horse has crossed the upper or loewr limit
-            print(f"Ops limit transpassed: {verti_coord}")
+        elif (ord(verti_coord) > 56 or ord(verti_coord) < 49):  # whether the horse has crossed the upper or lower limit
             continue
         else:
             possible_moves.append((coordinates))
@@ -100,17 +58,16 @@ class ChessBoard:
             index=[i + 1 for i in range(8)],
         )
 
-    def stamp_moves(self, initial_pos, moves: list) -> None:
-        self.board[initial_pos[0]][
-            int(initial_pos[1])
-        ] = "h"  # marking the horse start position
+    def stamp_moves(self, initial_pos, final_pos, moves: list) -> None:
+        self.board[initial_pos[0]][int(initial_pos[1])] = "h"  # marking the horse start position
         for move in moves:
-            self.board[move[0]][int(move[1])] = "o"
+            if move == final_pos:
+                self.board[move[0]][int(move[1])] = "*"
+            else:
+                self.board[move[0]][int(move[1])] = "o"
 
-    # getter method
     def get_board(self) -> "board":
         return self.board
-
 
 def main():
     initial_pos, final_pos = input().split()
@@ -118,9 +75,12 @@ def main():
     valid_moves = possible_moves(moves)
     # creating the chess board, stamping the moves on it and printing this board
     board = ChessBoard()
-    board.stamp_moves(initial_pos, valid_moves)
+    board.stamp_moves(initial_pos, final_pos, valid_moves)
     marked_places = board.get_board()
+    if final_pos in valid_moves:
+        print("O movimento é válido,\nA casa final pode ser alcançada pelo cavalo:\n")
+    else:
+        print("O movimento é inválido,\nObservamos que a casa final não pode ser alcançada pelo cavalo:\n")
     print(marked_places)
-
 
 main()
